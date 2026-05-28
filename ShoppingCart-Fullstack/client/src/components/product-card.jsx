ebb02e2ba693112f0f4ceb6b4beccafca9807a3f
Tu Nombre <22030946@itcelaya.edu.mx>
2026-05-28 11:39:10 -0600

    feat: imagenes y productos personalizados

--- ShoppingCart-Fullstack/client/src/components/product-card.jsx
+++ ShoppingCart-Fullstack/client/src/components/product-card.jsx
@@ -11,7 +11,7 @@ export const ProductCard = ({ image, name, category, price }) => {
         <source media="(min-width: 1440px)" srcSet={image.desktop} />
         <source media="(min-width: 768px)" srcSet={image.tablet} />
         <img
-          className={`rounded-lg mb-[38px] ${quantity > 0 ? 'border-2 border-Red' : ''}`}
+          className={`rounded-lg mb-[38px] w-full h-[200px] object-cover ${quantity > 0 ? 'border-2 border-Red' : ''}`}
           src={image.mobile}
           alt={name}
         />
@@ -20,13 +20,13 @@ export const ProductCard = ({ image, name, category, price }) => {
       {quantity === 0 ? (
         <button
           onClick={() => addToCart({ name, category, price, image })}
-          className='bg-Rose-50 border-2 border-Rose-300 w-40 rounded-full flex justify-center gap-2 p-3 absolute inset-x-0 mx-auto top-[190px] cursor-pointer hover:border-Red transition-colors'
+          className='bg-Rose-50 border-2 border-Rose-300 w-40 rounded-full flex justify-center gap-2 p-3 absolute inset-x-0 mx-auto top-[170px] cursor-pointer hover:border-Red transition-colors'
         >
           <img src='/assets/images/icon-add-to-cart.svg' alt='' />
           <span>Add to Cart</span>
         </button>
       ) : (
-        <div className='bg-Red w-40 rounded-full flex justify-between items-center p-3 absolute inset-x-0 mx-auto top-[190px]'>
+        <div className='bg-Red w-40 rounded-full flex justify-between items-center p-3 absolute inset-x-0 mx-auto top-[170px]'>
           <img onClick={() => decrement(name)} className='border border-Rose-50 size-[18px] rounded-full p-1 cursor-pointer' src='/assets/images/icon-decrement-quantity.svg' alt='' />
           <p className='text-Rose-50'>{quantity}</p>
           <img onClick={() => increment(name)} className='border border-Rose-50 size-[18px] rounded-full p-1 cursor-pointer' src='/assets/images/icon-increment-quantity.svg' alt='' />
